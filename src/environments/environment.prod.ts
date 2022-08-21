86fdb324fbde0e8ad06fb1c413bb4bbae453f070
Bryan Arias <brayank012@hotmail.com>
2022-08-21 11:24:01 -0500

    first commit

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  production: true
+  production: true,
+  mapBoxToken:
+    'pk.eyJ1IjoiYnJ5YW5kM3YiLCJhIjoiY2w2enZiMjRoMDg1cTN2cG16a2xndjNkcSJ9.Lz-AdM56KzNnYGzQOX43gA',
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,9 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  mapBoxToken:
+    'pk.eyJ1IjoiYnJ5YW5kM3YiLCJhIjoiY2w2enZiMjRoMDg1cTN2cG16a2xndjNkcSJ9.Lz-AdM56KzNnYGzQOX43gA',
 };
 
 /*
